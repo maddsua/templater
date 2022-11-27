8b310191457268f75ed0bd138199cd7965f3de39
maddsua <maddsuapro@gmail.com>
2022-11-28 01:26:12 +0200

    progress save 2

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,9 @@ import fs from 'fs';
 
 const regexes = {
 	template_var: /\{\{\s[\_A-Za-z]{1}[A-Za-z0-9\_]{0,}\s\}\}/g,
-	variable: /[\_A-Za-z]{1}[A-Za-z0-9\_]{0,}/g
+	variable: /[\_A-Za-z]{1}[A-Za-z0-9\_]{0,}/g,
+	inputFile: /^.*\.htm(l?)$/,
+	directory: /^.*\//
 }
 
 const configPath = './templater.config.json';
@@ -10,7 +12,27 @@ const configPath = './templater.config.json';
 const rootPathToLocalRoot = (path) => {
 	if (path[0] === '/' || path[0] === '\\') return (`.${path}`);
 	return path;
-}
+};
+
+const findAllFiles = (searchDir) => {
+
+	let results = [];
+
+	const dir_search = () => {	
+		if (!fs.existsSync(searchDir)) return;
+
+		fs.readdirSync(searchDir).forEach((file) => {
+			const filename = `${searchDir}/${file}`;
+			const stat = fs.lstatSync(filename);
+	
+			if (stat.isDirectory()) dir_search() 
+			else if (regexes.inputFile.test(filename)) results.push(filename);
+		})
+	};
+	dir_search();
+	
+	return results;
+};
 
 //	the main()
 (() => {
@@ -24,17 +46,30 @@ const rootPathToLocalRoot = (path) => {
 	}
 
 	const files = config['files'];
-		if (typeof files.length !== 'number') {
-			console.error('No files specified in config');
-			return false;
-		}
+	const sourceDir = config['sourceDir'];
+
+	let publicRoot = config['publicRoot'];
+		if (typeof publicRoot !== 'string') publicRoot = false;
+
+	let inputs = [];
+	//	add specifiend files
+	if (typeof files.length === 'number') {
+		files.forEach((file) => {
+			if (typeof file.from === 'string' && typeof file.to === 'string') inputs.push(file);
+		});
+	}
+	//	add files, that were found in src directories
+	if (typeof sourceDir === 'string' && publicRoot) {
+		const sources = findAllFiles(rootPathToLocalRoot(sourceDir));
+		console.log(sources);
+	}
+		
 	const variables = config['data'];
 		if (typeof variables !== 'object') {
 			console.error('No template data block in config');
 			return false;
 		}
-	let publicRoot = config['publicRoot'];
-		if (typeof publicRoot !== 'string') publicRoot = false;
+
 
 
 	const processTemplate = (templateText) => {
@@ -50,14 +85,12 @@ const rootPathToLocalRoot = (path) => {
 			tempHtml = tempHtml.replace(new RegExp(tempVar), variables[varname] || '');
 		});
 
-		if (publicRoot) {
-			tempHtml = tempHtml.replace(new RegExp(`${publicRoot}`, 'g'), '/');
-		}
+		if (publicRoot) tempHtml = tempHtml.replace(new RegExp(`${publicRoot}`, 'g'), '/');
 	
 		return tempHtml;
 	};
 
-	files.forEach(filepath => {
+	inputs.forEach(filepath => {
 
 		const srcfile = rootPathToLocalRoot(filepath.from);
 		const outfile = rootPathToLocalRoot(filepath.to);
@@ -70,7 +103,13 @@ const rootPathToLocalRoot = (path) => {
 			return;
 		}
 
+		const destDir = outfile.match(regexes.directory)[0];
+		const destFile = destDir.length > 1 ? outfile.substring(destDir.length) : outfile;
+	
+		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
+
 		fs.writeFileSync(outfile, processTemplate(htmltext), {encoding: 'utf8'});
-		
-	});	
+		console.log(`Processed ${srcfile}`);
+	});
+
 })();
