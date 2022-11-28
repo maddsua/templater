f62f9f21bb50d6467ad432c5285d6dd82669f820
maddsua <maddsuapro@gmail.com>
2022-11-28 02:58:35 +0200

    progress save 3

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,15 @@ import fs from 'fs';
 const regexes = {
 	template_var: /\{\{\s[\_A-Za-z]{1}[A-Za-z0-9\_]{0,}\s\}\}/g,
 	variable: /[\_A-Za-z]{1}[A-Za-z0-9\_]{0,}/g,
+	var_file: /^\$file\=/,
 	inputFile: /^.*\.htm(l?)$/,
 	directory: /^.*\//,
 	dirSlashes: /(\/\/)|(\\\\)|(\\)/g
 }
 
 const configPath = './templater.config.json';
+
+const watchMode = process.argv.find((arg) => (arg === '--watch' || arg === '-w')) ? true : false;
 
 const findAllFiles = (searchDir) => {
 
@@ -43,7 +46,10 @@ const separatePath = (path) => {
 
 const normalizePath = (path) => {
 	let temp = path.replace(regexes.dirSlashes, '/');
-	if (path[0] === '/' || path[0] === '\\') return (`.${temp}`);
+
+	if (temp[0] === '/') temp = temp.substring(1);
+	if (temp.slice(-1) === '/') temp = temp.slice(0, -1); 
+
 	return temp;
 };
 
@@ -59,6 +65,13 @@ const normalizePath = (path) => {
 		return false;
 	}
 
+	const trimPubRoot = config['trimPublicRoot'];
+
+	const variables = config['data'];
+	if (typeof variables !== 'object') {
+		console.error('No template data block in config');
+		return false;
+	}
 
 	//	deal with input files
 	const files = config['files'];
@@ -68,8 +81,9 @@ const normalizePath = (path) => {
 		if (typeof publicRoot !== 'string') publicRoot = false;
 
 	let inputs = [];
-	//	add specifiend files
-	if (typeof files.length === 'number') {
+
+	//	add specified files
+	if (typeof files?.length === 'number') {
 		files.forEach((file) => {
 			if (typeof file.from === 'string' && typeof file.to === 'string') inputs.push({
 				from: normalizePath(file.from),
@@ -77,13 +91,14 @@ const normalizePath = (path) => {
 			});
 		});
 	}
+
 	//	add files, that were found in src directories
 	if (typeof sourceDir === 'string' && publicRoot) {
 		findAllFiles(normalizePath(sourceDir)).forEach((filepath) => {
 			const file_from = normalizePath(filepath);
 			const file_to = normalizePath(`${publicRoot}/${separatePath(file_from).file}`);
 
-			if (!inputs.find((item) => (item.from == file_from && item.to === file_to))) inputs.push({
+			if (!inputs.find((item) => (item.from === file_from && item.to === file_to))) inputs.push({
 				from: file_from,
 				to: file_to
 			});
@@ -92,47 +107,81 @@ const normalizePath = (path) => {
 
 
 	// process templates
-		
-	const variables = config['data'];
-		if (typeof variables !== 'object') {
-			console.error('No template data block in config');
-			return false;
-		}
 
 	const processTemplate = (templateText) => {
-
-		//	insert text strings
+		//	the c language habits, completely unnecessary here
 		let tempHtml = templateText;
 
+		//	find all template literals?... or whatever you call that
 		const allTempVars = tempHtml.match(regexes.template_var);
-		allTempVars.forEach(tempVar => {
+		allTempVars?.forEach(tempVar => {
+			//	just to be sure
 			const varname = tempVar.match(regexes.variable)[0];
 				if (!varname) return;
+
+			//	find value or return empty string
+			let dataValue = variables[varname] || '';
+
+				//	data post processing
+				if (dataValue.length > 0) {
+
+					//	load file contents from the path, specifiend in the string
+					if (regexes.var_file.test(dataValue)) {
+						const insertFilePath = normalizePath(dataValue.replace(regexes.var_file, ''));
+						try {
+							dataValue = fs.readFileSync(insertFilePath, {encoding: 'utf8'}).toString();
+						} catch (error) {
+							dataValue = '';
+						}
+					}
+				}
 	
-			tempHtml = tempHtml.replace(new RegExp(tempVar), variables[varname] || '');
+			//	insert text to html document
+			tempHtml = tempHtml.replace(new RegExp(tempVar), dataValue);
 		});
 
-		if (publicRoot) tempHtml = tempHtml.replace(new RegExp(`${publicRoot}`, 'g'), '/');
+		//	trim public folder path
+		if (publicRoot && trimPubRoot) tempHtml = tempHtml.replace(new RegExp(`/${normalizePath(publicRoot)}/`, 'g'), '/');
 	
 		return tempHtml;
 	};
 
-	inputs.forEach(filepath => {
-
-		let htmltext = {};
+	const processFileTemplate = (srcpath, destpath) => {
+		let htmltext = '';
 		try {
-			htmltext = fs.readFileSync(filepath.from, {encoding: 'utf8'}).toString();
+			htmltext = fs.readFileSync(srcpath, {encoding: 'utf8'}).toString();
 		} catch (error) {
-			console.error(`Can't load template file ${filepath.from}, error: ${error}`);
-			return;
+			console.error(`Can't load template file ${srcpath}, error: ${error}`);
+			return false;
 		}
 
-		const destDir = separatePath(filepath.to).dir;
+		const destDir = separatePath(destpath).dir;
 	
 		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
 
-		fs.writeFileSync(filepath.to, processTemplate(htmltext), {encoding: 'utf8'});
-		console.log(`Processed ${filepath.from}`);
+		fs.writeFileSync(destpath, processTemplate(htmltext), {encoding: 'utf8'});
+		console.log(`Processed '${srcpath}'`);
+
+		return true;
+	};
+
+	inputs.forEach(filepath => {
+		processFileTemplate(filepath.from, filepath.to);
+
+		if (watchMode) {
+			const watchdog = fs.watch(filepath.from, (eventType, filename) => {
+				console.log('\r\n');
+				if (eventType === 'change') {
+					console.log(`Rebuilding '${filename}'`);
+					processFileTemplate(filepath.from, filepath.to);
+					return;
+				}
+				console.log(`File '${filename}' was renamed or moved`);
+				watchdog.close();
+			});
+		}
 	});
+
+	if (watchMode) console.log('Waiting for file changes...');
 
 })();
---
+++ readme.md
@@ -0,0 +1,14 @@
+{
+	"files": [
+		{
+			"from": "/test/source.htm",
+			"to": "/test/public.html"
+		}
+	],
+	"sourceDir": "test_src",
+	"publicRoot": "test_public",
+
+	"data": {
+		"vari1": "Text from var 1"
+	}
+}
